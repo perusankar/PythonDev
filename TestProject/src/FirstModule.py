f1880bcb1227e520e619cb3268c3e53cf83e5b0c
iyyamperumal@hotmail.com <iyyamperumal@hotmail.com>
2017-07-08 14:32:06 -0400

    change version

--- TestProject/src/FirstModule.py
+++ TestProject/src/FirstModule.py
@@ -9,12 +9,29 @@ def add(a,b):
 def addFixedValue(a):
     y = 5
     return y +a
+'''
+Multiply Function
+'''
+def multiply(a,b):
+    return a*b
+'''
+Divide Function
+'''
+def divide(a,b):
+    return a/b
 
-print("Test")
-print(add(1,2))
+#Print statements
+print("Start")
+print("Add " ,add(1,2))
 print(addFixedValue(1))
-print("Test")
-print("Test2")
-print("Test4")
-print("Test3")
-print("Test6")
+print("Multiply " ,multiply(4,6))
+#raise IOError("file error")
+print("Divide ",divide(5,25))
+#x = input('something:') 
+#print('Test ',x, end=" ")
+print("End")
+
+if True:
+    print("true")
+else:
+    print("true")
